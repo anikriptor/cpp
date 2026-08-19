92ca2c857d3b34b1d9c471a2da622f880d2f68fc
Sinful Bobcat <bobcatsinful@gmail.com>
2026-08-19 12:51:35 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+
+  std::cout << "Hello mom!\n";    //this is a comment
+                    //returns no error
+
+  std::cout << -6.7;
+  /*
+   * this
+   * is 
+   * a 
+   * multi 
+   * line 
+   * comment 
+  */
+
+  return 0;
+
+}
